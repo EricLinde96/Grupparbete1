e760cca2cfed392a567518e18a31343dbc3e874a
Eric Linde <ericlinde19@gmail.com>
2025-10-21 12:34:59 +0200

    La till nr 1 Singed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
 public static void main (String[] args){
-System.out.println("Hello from person A and B");
+System.out.println("Hello from person A(NR 1 SINGED BABY and B");
 }
 
     }
